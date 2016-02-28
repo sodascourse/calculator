2e6c2afdde52733b97851d0411bf976f15da578c
sodastsai <sodas2002@gmail.com>
2016-02-28 23:44:48 +0800

    Init commit

---
+++ Calculator/DisplayLabel.swift
@@ -0,0 +1,21 @@
+//
+//  DisplayLabel.swift
+//  Calculator
+//
+//  Created by sodas on 2/28/16.
+//  Copyright © 2016 sodas. All rights reserved.
+//
+
+import UIKit
+
+class DisplayLabel: UILabel {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Calculator
+
+This example is also part of the homework 1. So some features are not all implemented. (Leave as homework)
