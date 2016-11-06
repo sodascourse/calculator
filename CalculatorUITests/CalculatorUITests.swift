43b5abaa1ea8ef92e7fb1016978effd79b1c88e0
sodastsai <sodas2002@gmail.com>
2016-11-06 17:24:27 +0800

    Add test

--- CalculatorUITests/CalculatorUITests.swift
+++ CalculatorUITests/CalculatorUITests.swift
@@ -9,11 +9,42 @@
 import XCTest
 
 class CalculatorUITests: XCTestCase {
+
+    var app: XCUIApplication!
         
     override func setUp() {
         super.setUp()
         self.continueAfterFailure = false
-        XCUIApplication().launch()
+        self.app = XCUIApplication()
+        self.app.launch()
+    }
+
+    func testAdd() {
+        self.app.buttons["1"].tap()
+        self.app.buttons["+"].tap()
+        self.app.buttons["5"].tap()
+        self.app.buttons["="].tap()
+        XCTAssertTrue(self.app.staticTexts["6"].exists)
+    }
+
+    func testSub() {
+        self.app.buttons["2"].tap()
+        self.app.buttons["0"].tap()
+        self.app.buttons["−"].tap()
+        self.app.buttons["3"].tap()
+        self.app.buttons["="].tap()
+        XCTAssertTrue(self.app.staticTexts["17"].exists)
+    }
+
+    func testReset() {
+        self.app.buttons["2"].tap()
+        self.app.buttons["0"].tap()
+        self.app.buttons["AC"].tap()
+        self.app.buttons["2"].tap()
+        self.app.buttons["−"].tap()
+        self.app.buttons["3"].tap()
+        self.app.buttons["="].tap()
+        XCTAssertTrue(self.app.staticTexts["-1"].exists)
     }
 
 }
